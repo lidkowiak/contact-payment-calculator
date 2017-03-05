37cbd9de96e23700e939d204c457c193c8ef1cdd
lidkowiak <lukasz.idkowiak@gmail.com>
2017-03-05 01:18:45 +0100

    validation and error handling in UI

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -4,30 +4,41 @@ var app = new Vue({
         supportedCountryTaxSystems: [],
         selectedCountryIndex: 0,
         dailyNetSalary: null,
-        monthlyPlnNetContractSalary: 0
+        dailyNetSalaryError: false,
+        monthlyPlnNetContractSalary: {},
+        errorResponse: null
     },
-    mounted : function() {
-        axios.get('/api/country-tax-systems', { headers : {'Content-Type' : 'application/json;charset=UTF-8'}, data : {}})
-                  .then(function (response) {
-                   app.supportedCountryTaxSystems = response.data;
-                  })
-                  .catch(function (error) {
-                    console.log(error);
-                  });
-    },
-    methods : {
-        calculateMonthlyPlnNetContractSalary : function() {
-            var url = '/api/country-tax-systems/' + app.supportedCountryTaxSystems[app.selectedCountryIndex].countryCode + '/monthly-pln-net-contract-salary-calculation';
-            axios.post(url, {
-                    amount : app.dailyNetSalary,
-                    currency : app.supportedCountryTaxSystems[app.selectedCountryIndex].currencyCode
+    mounted: function() {
+        axios.get('/api/country-tax-systems', { headers: { 'Content-Type': 'application/json;charset=UTF-8' }, data: {} })
+            .then(function(response) {
+                app.supportedCountryTaxSystems = response.data;
             })
-            .then(function (response) {
-                app.monthlyPlnNetContractSalary = response.data;
-            })
-            .catch(function (error) {
-                console.log(error);
+            .catch(function(error) {
+                app.errorResponse = error.response.data;
             });
+    },
+    methods: {
+        calculateMonthlyPlnNetContractSalary: function() {
+            app.errorResponse = null;
+            app.monthlyPlnNetContractSalary = {};
+
+            if (!app.dailyNetSalary) {
+                app.dailyNetSalaryError = true;
+                return;
+            }
+            app.dailyNetSalaryError = false;
+            var url = '/api/country-tax-systems/' + app.supportedCountryTaxSystems[app.selectedCountryIndex].countryCode + '/monthly-pln-net-contract-salary-calculation';
+            var payload = {
+                amount: app.dailyNetSalary,
+                currency: app.supportedCountryTaxSystems[app.selectedCountryIndex].currencyCode
+            };
+            axios.post(url, payload)
+                .then(function(response) {
+                    app.monthlyPlnNetContractSalary = response.data;
+                })
+                .catch(function(error) {
+                    app.errorResponse = error.response.data;
+                });
         }
     }
 });
